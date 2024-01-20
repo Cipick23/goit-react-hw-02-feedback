57d798832d2c0bd01703b92159f7e5218141177f
Cipick23 <133102658+Cipick23@users.noreply.github.com>
2024-01-20 17:40:14 +0200

    final commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,53 @@
-import { Component } from 'react';
-import Feedback from './feedback/Feedback';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Statistics from './statistics/Statistics';
+import FeedbackOptions from './feedback options/FeedbackOptions';
+import Section from './section/Section';
+import Notification from './notification/Notification';
+import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleFeedback = (type) => {
+    this.setState((prevState) => ({ [type]: prevState[type] + 1 }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const total = this.countTotalFeedback();
+    return total ? Math.round((this.state.good / total) * 100) : 0;
+  };
+
   render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+
     return (
       <div>
-        {/* Feedback Widget */}
-        <Feedback />
-
+        <Section title="Feedback Widget">
+          <FeedbackOptions options={['good', 'neutral', 'bad']} onLeaveFeedback={this.handleFeedback} />
+        </Section>
+        {total > 0 && (
+          <Section title="Statistics Widget">
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
+            />
+          </Section>
+        )}
+        {total === 0 && <Notification message="There is no feedback" />}
       </div>
     );
   }
